35261af4afb00357d8b66f8c321432523b814abb
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-06-03 23:34:53 +0300

    Finished ex 2.26

---
+++ chapter2/2.2/ex2.26.scm
@@ -0,0 +1,16 @@
+;; Exercise 2.26
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(newline)
+(display (append x y))
+;; (1 2 3 4 5 6)
+
+(newline)
+(display (cons x y))
+;; ((1 2 3) (4 5 6))
+
+(newline)
+(display (list x y))
+;; ((1 2 3) (4 5 6))
