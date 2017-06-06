727124725c4614b33c5a9b01392e7b34574f7941
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-06-06 23:57:43 +0300

    Finished ex2.33

---
+++ chapter2/2.2/ex2.33.scm
@@ -0,0 +1,31 @@
+;; Exercise 2.33
+
+(load "seq-interfaces.scm")
+
+(define (map p sequence)
+  (accumulate
+   (lambda (x y) (cons (p x) y))
+   nil
+   sequence))
+
+;; test1
+(newline)
+(display (map square (list 1 2 3 4)))
+(newline)
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+;;test2
+(newline)
+(display (append (list 1 3 4) (list 5 7 8)))
+(newline)
+
+(define (length sequence)
+  (accumulate
+   (lambda(x y)(+ y() 1))
+   0
+   sequence))
+
+;; test3
+(display (length (list 1 3 4)))
