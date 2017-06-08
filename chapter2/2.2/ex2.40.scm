431261a0df92bdfef732ee3f22af7df21bf40dec
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-06-08 21:51:45 +0300

    Finished ex2.40

---
+++ chapter2/2.2/ex2.40.scm
@@ -0,0 +1,19 @@
+;; Exercise 2.40
+
+(load "nested-mappings.scm")
+
+(define (unique-pairs n)
+  (accumulate
+   append
+   nil
+   (map (lambda (i)
+          (map (lambda (j)
+                 (list i j))
+               (enumerate-interval 1 (- i 1))))
+        (enumerate-interval 1 n))))
+
+(define (prime-sum-pairs-alt n)
+  (map make-pair-sum
+       (filter
+        prime-sum?
+        (unique-pairs n))))
