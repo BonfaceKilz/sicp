dbb433e6dd57cd6699599c1c9ae01a35436a540b
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-06-14 18:15:24 +0300

    Quotations in symbols

---
+++ chapter2/2.3/quotation.scm
@@ -0,0 +1,18 @@
+(define a 1)
+(define b 2 )
+
+(newline)
+
+(display (list a b))
+(newline)
+
+(display (list 'a 'b))
+(newline)
+
+(display (list 'a b))
+(newline)
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
