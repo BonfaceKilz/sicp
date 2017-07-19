7c6f5e4c3c25f6d09923f5ec17af321e2f322e7b
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-07-19 17:22:25 +0300

    Finished ex2.67

---
+++ chapter2/2.3/ex2.67.scm
@@ -0,0 +1,22 @@
+;; Exercise 2.67
+;; Defining an encodeing tree and a sample message
+
+(load "huffman-trees.scm")
+
+(define sample-tree
+  (make-code-tree
+   (make-leaf 'A 4)
+   (make-code-tree
+    (make-leaf 'B 2)
+    (make-code-tree
+     (make-leaf 'D 1)
+     (make-leaf 'C 1)))))
+
+(define sample-message
+  '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;; Decoding the message
+
+(decode sample-message sample-tree)
+
+;; Answer: A D A B B C A
