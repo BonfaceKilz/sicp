f8b9fd46368c0649221e32e5316a8c0b9c52cac9
BonfaceKilz <bonfacemunyoki@gmail.com>
2017-07-23 13:57:20 +0300

    Adding changed files

--- chapter2/2.4/data-directed-programming.scm
+++ chapter2/2.4/data-directed-programming.scm
@@ -1,6 +1,8 @@
 ;; Data-Directed Programming and Additivity
 
+;; Ben's package
 (define (install-rectangular-package)
+  ;; internal procedures
   (define (real-part z) (car z))
   (define (imag-part z) (cdr z))
   (define (make-from-real-imag x y)
@@ -26,3 +28,58 @@
        (lambda (r a)
          (tag (make-from-mag-ang r a))))
   'done)
+
+;; Alyssa's package
+(define (install-polar-packages)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y)
+         (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a)
+         (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error
+           "No method for these types: APPLY-GENERIC"
+           (list op type-tags))))))
+
+;; Defining Generic selectors
+(define (real-part z)
+  (apply-generic 'real-part z))
+(define (imag-part z)
+  (apply-generic 'imag-part z))
+(define (magnitude z)
+  (apply-generic 'magnitude z))
+(define (angle z)
+  (apply-generic 'angle z))
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag
+        'rectangular)
+   x y))
+(define (make-from-mag-ang x y)
+  ((get 'make-from-mag-ang
+        'polar)
+   r a))
