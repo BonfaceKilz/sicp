e4ba69407e8f2a091c8c49d44cd900e4079610ef
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-01 01:08:44 +0300

    Move functions locally scoped to `simpson` function

--- chapter1/1.3/ex1.29.scm
+++ chapter1/1.3/ex1.29.scm
@@ -1,20 +1,22 @@
-(define (cube x) (* x x x))
 ;;; (Exercise 1.29) --- Simpson's Rule
 
-(define (inc n) (+ n 1))
+;; Using Simpson's Rule to compute integrals
 
-(define (odd? n)
-  (> (remainder n 2) 0))
-
-(define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next a) next b))))
-
+;;; Code:
 (define (simpson f a b n)
+  "Numerical integration using Simpson's rule"
+  (define (inc n) (+ n 1))
+
+  (define (sum term a next b)
+    (if (> a b)
+        0
+        (+ (term a)
+           (sum term (next a) next b))))  
+
   (define h (/ (- b a) n))
+
   (define (yk k)(f (+ a (* h k))))
+
   (define (simpson-term k)
     (* (cond ((or (= k 0) (= k n)) 1)
              ((odd? k) 4)
@@ -24,6 +26,7 @@
      (sum simpson-term 0 inc n)))
 
 ;; Tests
+(define (cube x) (* x x x))
 (simpson cube 0 1 100)
 (simpson cube 0 1 1000)
 
