4ac66220c43eca3876e761d12f7ee7bacc74a5a1
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-01 11:22:00 +0300

    Properly write the filtered-accumulate function ex1.33

--- chapter1/1.3/ex1.33.scm
+++ chapter1/1.3/ex1.33.scm
@@ -1,32 +1,67 @@
-;; Exercise 1.33
+;;; (Exercise 1.33) --- Filter function
 
-(load "ex1.32.scm")
+;; Introducing the notion of a filter on the terms to be combined.
 
-;; Recursive form of filtered version
-(define (filtered-accumulate combiner null-value term a next b filter)
+;;; Code:
+(define (filtered-accumulate filter combiner null-value term a next b)
   (if (> a b)
       null-value
-      (if (filter a)
-          (combiner (term a)
-                    (filtered-accumulate combiner null-value term (next a) next b filter))
-          (combiner null-value (filtered-accumulate combiner null-value term (next a) next b filter)))))
-
-;; Another form
-(define (filtered-accumulator filter combiner null-value term a next b)
-  (if (> a b)
-      null-value
-      (combiner (if (filter a) (term a)
+      (combiner (if (filter a)
+                    (term a)
                     null-value)
-                (filtered-accumulator filter combiner null-value term (next a) next b))))
+                (filtered-accumulate filter combiner null-value term (next a) next b))))
 
-;; An iterative version
-(define (filtered-accumulate combiner null-value term a next b filter)
+(define (filtered-accumulate-iter combiner null-value term a next b filter)
   (define (iter a result)
     (cond ((> a b) result)
           ((filter a) (iter (next a) (combiner result (term a))))
           (else (iter (next a) result))))
   (iter a null-value))
 
-(define (define (next next x) (+ x 1)))
+;; Demos:
+(define (prime? n)
+  "Check whether a number is prime"
+  (define (square x) (* x x))
 
-(define (sum-prime-squares a b))
+  (define (divides? a b)
+    (= (remainder b a) 0))
+
+  (define (smallest-divisor n)
+    (find-divisor n 2))
+
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n)n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+  (and (not (= n 1))
+       (= n (smallest-divisor n))))
+
+(define (sum-prime-squares a b)
+  (filtered-accumulate prime? ; filter
+                       + ; combiner
+                       0 ; null-value
+                       (lambda (x) x) ; term
+                       a ; first val
+                       (lambda (x) (+ 1 x)) ; next function
+                       b ; final value
+                       ))
+
+(define (product-relative-prime n)
+  "Calculate the product of all +ve integers less than n that are relatively
+prime to n"
+  (define (gcd a b)
+    (if (= b 0)
+        a
+        (gcd b (remainder a b))))
+
+  (filtered-accumulate (lambda (x) (= (gcd x n) 1)) ; filter
+                       ,* ; combiner
+                       1 ; null-value
+                       (lambda (x) x) ; term
+                       1 ; first val
+                       (lambda (x) (+ 1 x)) ; next function
+                       (- n 1) ; final value
+                       ))
+
+;;; Exercise 1.33 ends here
