ade91592ae295b0e09caa9e38889d93b52fdc3e5
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-01 12:43:19 +0300

    Update ex1.41

--- chapter1/1.3/ex1.41.scm
+++ chapter1/1.3/ex1.41.scm
@@ -1,14 +1,14 @@
-;; Exercise 1.41
-;; Defining a procedure double that takes a
-;; procedure of one argument and returns a
-;; procedure that applies the original procedure
-;; twice
+;;; (Exercise 1.41) --- Approximating zeros of cubic functions
 
+;; Defining a procedure double that takes a procedure of one argument and
+;; returns a procedure that applies the original procedure twice
+
+;;; Code:
 (define (double f)
   (lambda (x)
     (f (f x))))
 
-;; Example
+;; Demo:
 (define (inc x)(+ x 1))
-
-(((double (double double))inc) 5)
+(display ((double (double inc)) 1))
+;;; Exercise 1.41 ends here
