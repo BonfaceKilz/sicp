37c24598c97dea7d72f38fff7b6c65e96834c221
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-01 11:41:42 +0300

    Update ex1.36- add fixed-point fn in the file

--- chapter1/1.3/ex1.35.scm
+++ chapter1/1.3/ex1.35.scm
@@ -1,11 +1,26 @@
-;; Exercise 1.35
+;;; (Exercise 1.35) --- Computing 𝟇
 
-;; Showing that the golden ration 𝟇(1.2.2) is a fixed point of
-;; transformation x -> 1 + 1/x
+;; Showing that the golden ratio: 𝟇(1.2.2) is a fixed point of transformation x
+;; -> 1 + 1/x
+;; The golden ratio is the constant: 1.6180327868852458
 
-(load "fixed-points.scm")
+;; Calculating fixed points
+(define (fixed-point f first-guess)
+  "Calculating fixed points"
+  (let ((tolerance 0.00001))
+    (define (close-enough? v1 v2)
+      (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+      (let ((next (f guess)))
+        (if (close-enough? guess next)
+            next
+            (try next))))
+
+    (try first-guess)))
+
 
 (define (golden-ratio x)
   (fixed-point (lambda (x)
                  (+ 1.0 (/ 1.0 x)))
                1))
+;;; Exercise 1.35 ends here
