c9af95974b162ee816884727f005addcf3f43cae
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-03-01 12:24:56 +0300

    Updating ex1.38
    Estimating the value of e

--- chapter1/1.3/ex1.38.scm
+++ chapter1/1.3/ex1.38.scm
@@ -1,15 +1,26 @@
-;; Exercise 1.38
-;; De Fractionibus Continuis
+;;; (Exercise 1.38) --- De Fractionibus Continuis
 
-(load "ex1.37.scm")
+;; In 1737, the Swiss mathematician Leonhard Euler published a memoir "De
+;; Fractionibus Continuis", which included a continued fraction for e - 2, where
+;; e is the base of the natural logarithms. In this fraction, Ni are all 1, and
+;; the Di are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, ...; This program
+;; approximates e, based on Euler's expansion
 
-(define (numerator k)
-  (+ 1))
-
-(define (denominator k)
-  (if (= (remainder (+ k 1) 3) 0)
-      (/ (+ k 1) 1.5)
-      1))
+(define (continued-fraction n d k)
+  "Calculating continued fractions using an iterative process"
+  (define (iter i result)
+    (if (= i 1)
+        (/ (n i) (+ (d i) result))
+        (iter (- i 1) (/ (n i)
+                         (+ (d i) result)))))
+  (iter k 1))
 
 (define (euler k)
-  (+ 2.0 (cont-frac numerator denominator k)))
+  "Estimating the value of e"
+  (+ 2.0 (continued-fraction
+          (lambda (i) 1.0)
+          (lambda (i) (if (= (remainder (+ i 1) 3) 0)
+                          (/ (+ k 1) 1.5)
+                          1))
+          k)))
+;;; Exercise 1.38 ends here
